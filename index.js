b2224a353a233baa32087a4b5073de3bd02d5e42
minhman2610 <49876209+minhman2610@users.noreply.github.com>
2019-04-22 23:14:19 +0700

    update token

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-// example from http://howtonode.org/hello-node
-// Load the http module to create an http server.
-const http = require('http')
-const message = 'Hello World from Node\n'
+// # SimpleServer
+// A simple chat bot server
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer(function (request, response) {
-  console.log('responding with hello')
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(message)
-})
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+var request = require("request");
 
-const port = process.env.PORT || 1337
-server.listen(port)
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
 
-console.log('Server running at port', port)
-console.log(message)
+// Đây là đoạn code để tạo Webhook
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'maiviky') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+// Xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+
+// Gửi thông tin tới REST API để trả lời
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "EAAKIj89yTb4BAL9rxEKipnVjYyAI7g89hImPo7RQAMfVdC1Mx3cF5FYVnXwv3RDttcqRwOKPfSpVWaNP0ogujaqGNuxi5WvVdNMwrVtZC5W0SCtEE1K8KmeYpQZA2uvmle0Df7bwz6txzYc9iUlBYILE13UpXEtEVZCWwJAZBq1Q96q5UimLFx0MtFW0Ij8ZD",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
