d824792f52a0d830091b34ef8e2ca76009c5b8c2
minhman2610 <49876209+minhman2610@users.noreply.github.com>
2019-04-22 23:34:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// example from http://howtonode.org/hello-node
+// Load the http module to create an http server.
+const http = require('http')
+const message = 'Hello World from Node\n'
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer(function (request, response) {
+  console.log('responding with hello')
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end(message)
+})
+
+const port = process.env.PORT || 1337
+server.listen(port)
+
+console.log('Server running at port', port)
+console.log(message)
