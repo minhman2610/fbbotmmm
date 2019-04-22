58d6e84881081857e2cdb86862fc06be72c7a7ac
minhman2610 <49876209+minhman2610@users.noreply.github.com>
2019-04-22 23:38:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ const message = 'Hello World from Node\n'
 
 console.log('Server running at port', port)
 console.log(message+'----'+port)
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer(function (request, response) {
+  console.log('responding with hello')
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end(message+'---'+port)
+})
+
