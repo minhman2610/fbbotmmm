c001d4b392333ac1235a25ed5bb722c65224d465
minhman2610 <49876209+minhman2610@users.noreply.github.com>
2019-04-22 23:51:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-// example from http://howtonode.org/hello-node
-// Load the http module to create an http server.
-const http = require('http')
-const message = 'Hello World from Node\n'
+'use strict';
 
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer(function (request, response) {
-  console.log('responding with hello')
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(message)
-})
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-const port = process.env.PORT || 1337
-server.listen(port)
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-console.log('Server running at port', port)
-console.log(message)
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
 
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "maiviky"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
